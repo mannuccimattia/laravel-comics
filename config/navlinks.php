036f85ebc9d4d9cd74eae52687eba5c4d07ad4eb
mannuccimattia <mannuccimattia@gmail.com>
2025-07-28 12:47:58 +0200

    [UPD] Done header - static links

---
+++ config/navlinks.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+  [
+    "id" => 1,
+    "href" => "#",
+    "label" => "Characters",
+    "current" => false
+  ],
+  [
+    "id" => 2,
+    "href" => "#",
+    "label" => "Comics",
+    "current" => true
+  ],
+  [
+    "id" => 3,
+    "href" => "#",
+    "label" => "Movies",
+    "current" => false
+  ],
+  [
+    "id" => 4,
+    "href" => "#",
+    "label" => "TV",
+    "current" => false
+  ],
+  [
+    "id" => 5,
+    "href" => "#",
+    "label" => "Games",
+    "current" => false
+  ],
+  [
+    "id" => 6,
+    "href" => "#",
+    "label" => "Collectibles",
+    "current" => false
+  ],
+  [
+    "id" => 7,
+    "href" => "#",
+    "label" => "Videos",
+    "current" => false
+  ],
+  [
+    "id" => 8,
+    "href" => "#",
+    "label" => "Fans",
+    "current" => false
+  ],
+  [
+    "id" => 9,
+    "href" => "#",
+    "label" => "News",
+    "current" => false
+  ],
+  [
+    "id" => 10,
+    "href" => "#",
+    "label" => "Shop",
+    "current" => false
+  ]
+];
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,23 @@
-<div class="container-fluid">
-    <header>
-        <nav class="navbar">
-            <div>logo</div>
-            <div class="nav-link">link</div>
-            <div class="nav-link">link</div>
-            <div class="nav-link">link</div>
-            <div class="nav-link">link</div>
+@php
+    $links = config('navlinks');
+@endphp
+
+<header>
+    <div class="container">
+        <div>
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC logo">
+        </div>
+        <nav>
+            <ul>
+                @foreach ($links as $link)
+                    <li>
+                        <a href="{{ $link['href'] }}" class="{{ $link['current'] ? 'active' : '' }}">
+                            {{ $link['label'] }}
+                        </a>
+                        <div class="mark"></div>
+                    </li>
+                @endforeach
+            </ul>
         </nav>
-    </header>
-</div>
+    </div>
+</header>
