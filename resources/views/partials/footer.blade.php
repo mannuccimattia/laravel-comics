9f55d3f2acde8340cf92057eff0e3bbfb6d8379e
mannuccimattia <mannuccimattia@gmail.com>
2025-07-28 13:48:41 +0200

    [UPD] Done footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,107 @@
-<div class="container-fluid">
-    <footer>
-        footer
-    </footer>
-</div>
+<footer>
+    <section class="top-footer">
+        <div class="container">
+            <div class="content">
+                <div class="footer-col left">
+                    <div class="footer-subcol">
+                        <h3>DC COMICS</h3>
+                        <ul>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Tv</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Collectibles</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">Fans</a></li>
+                            <li><a href="#">News</a></li>
+                            <li><a href="#">Shop</a></li>
+                        </ul>
+                        <h3>SHOP</h3>
+                        <ul>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-subcol">
+                        <h3>DC</h3>
+                        <ul>
+                            <li><a href="#">Terms of Use</a></li>
+                            <li><a href="#">Privacy policy &#40;New&#41;</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-subcol">
+                        <h3>SITES</h3>
+                        <ul>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="footer-col right">
+                    <figure></figure>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="bottom-footer">
+        <div class="container">
+            <div class="content">
+                <div class="footer-col">
+                    <a href="#">
+                        <h3>SIGN-UP NOW!</h3>
+                    </a>
+                </div>
+                <div class="footer-col">
+                    <div class="wrapper">
+                        <h3>FOLLOW US</h3>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}"
+                                        alt="logo facebook" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}"
+                                        alt="logo twitter" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}"
+                                        alt="logo youtube" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}"
+                                        alt="logo pinterest" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}"
+                                        alt="logo periscope" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</footer>
